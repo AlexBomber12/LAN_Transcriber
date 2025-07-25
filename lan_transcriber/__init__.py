a5926a33fd0d04385564aa091e6cc2b4eb536f43
Alex_Bomber <48256657+AlexBomber12@users.noreply.github.com>
2025-07-25 16:52:28 +0200

    Fix tests and dependencies

--- lan_transcriber/__init__.py
+++ lan_transcriber/__init__.py
@@ -4,6 +4,9 @@ from .llm_client import LLMClient, generate
 from .pipeline import Settings, run_pipeline, Diariser
 from .models import SpeakerSegment, TranscriptResult
 from .normalizer import dedup
+from . import aliases, metrics
+from .aliases import *
+from .metrics import *
 
 __all__ = [
     "LLMClient",
@@ -14,4 +17,6 @@ __all__ = [
     "SpeakerSegment",
     "TranscriptResult",
     "dedup",
+    *aliases.__all__,
+    *metrics.__all__,
 ]
